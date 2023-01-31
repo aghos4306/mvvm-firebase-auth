5851b81c0348f3b8434d7e41be374a3474db4095
Aghogho <aghogho.akpedeye@gmail.com>
2023-01-31 16:53:57 +0000

    refactor mainactivity and create apptheme

--- app/src/main/java/com/aghogho/mvvmfirebaseauthcompose/MainActivity.kt
+++ app/src/main/java/com/aghogho/mvvmfirebaseauthcompose/MainActivity.kt
@@ -1,32 +1,40 @@
 package com.aghogho.mvvmfirebaseauthcompose
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.activity.viewModels
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
+import com.aghogho.mvvmfirebaseauthcompose.navigation.AppNavHost
+import com.aghogho.mvvmfirebaseauthcompose.ui.theme.AppTheme
 import com.aghogho.mvvmfirebaseauthcompose.ui.theme.MVVMFirebaseAuthComposeTheme
+import com.aghogho.mvvmfirebaseauthcompose.ui.viewmodel.AuthViewModel
 
 class MainActivity : ComponentActivity() {
+
+    private val authViewModel by viewModels<AuthViewModel>()
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("RememberReturnType", "UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MVVMFirebaseAuthComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            val snackbarHostState = remember { SnackbarHostState() }
+            Scaffold(
+                snackbarHost = { SnackbarHost(snackbarHostState) },
+                content = {
+                    AppTheme {
+                        AppNavHost()
+                    }
                 }
-            }
+            )
         }
     }
+
 }
 
 @Composable
--- app/src/main/java/com/aghogho/mvvmfirebaseauthcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/aghogho/mvvmfirebaseauthcompose/ui/theme/Theme.kt
@@ -1,10 +1,14 @@
 package com.aghogho.mvvmfirebaseauthcompose.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.res.colorResource
+import androidx.lifecycle.ktx.R
 
 private val DarkColorPalette = darkColorScheme(
     primary = Purple200,
@@ -44,4 +48,26 @@ fun MVVMFirebaseAuthComposeTheme(
         shapes = Shapes,
         content = content
     )
+}
+
+
+
+@Composable
+fun AppTheme(
+    useDarkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+    val colors = if (!useDarkTheme) {
+
+    } else {
+
+    }
+
+    CompositionLocalProvider(LocalSpacing provides Spacing()) {
+        MaterialTheme(
+            colorScheme = colorResource(id = com.aghogho.mvvmfirebaseauthcompose.R.color.white) as ColorScheme,
+            content = content,
+            typography = Typography
+        )
+    }
 }
